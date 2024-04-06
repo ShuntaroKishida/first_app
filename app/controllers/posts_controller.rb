0847255caffae299ef66094406abfea034f0e4c3
ShuntaroKishida <manhattan_midnight_2015@yahoo.co.jp>
2024-04-06 13:23:46 +0900

    Ajax実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,10 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all()
-    end
-
-    def new
-
+        @posts = Post.order(id: "DESC")
     end
 
     def create
-        Post.create(content: params[:content])
-        redirect_to "/posts"
+        post = Post.create(content: params[:content])
+        render json:{ post: post }
     end
 end
